4eeedfe2dbdae991874a8a7d6e287f49e339e408
Daniil Karagulin <44875599+Dannilich@users.noreply.github.com>
2025-04-28 12:55:22 +0300

    Add files via upload

--- Fold.rkt
+++ Fold.rkt
@@ -1,20 +1,38 @@
-;
+;Задания по функции Fold 
 #lang racket
 
+ 
 (define (fold f a n)
   (if (zero? n)
       a
       (f (- n 1) (fold f a (- n 1)))))
 
+
+;Функция многократного применения (или как кратность применения)
+(define (apply-n f x n)
+  (if (zero? n)
+      x
+      (apply-n f (f x) (- n 1))))
+
+;Реализуем через неё fold
+(define (fold-by-apply-n f a n)
+  (define (accum pair)
+    (cons (+ 1 (car pair)) (f (car pair) (cdr pair))))
+  (cdr (apply-n accum (cons 0 a) n)))
+
+
+;Реализация функций через fold
+
+;Факториал
 (define (factorial n)
-  (fold  (lambda (x y) (if (zero? x) 1 (* (+ 1 x) y)))
+  (fold-by-apply-n  (lambda (x y) (if (zero? x) 1 (* (+ 1 x) y)))
         1
         n))
 
+;Возведение в степень
 ;Выражено с помощью лямбда исчисления: \n.\m.\x.\y.n(m(x y))
 (define (pow m n)
-  ((fold (lambda (a b) (lambda (x) (* x (b x))))
+  ((fold-by-apply-n (lambda (a b) (lambda (x) (* x (b x))))
          (lambda (x) 1)
           n)
     m))
-
